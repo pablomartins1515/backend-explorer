d050dc33b4ab0d6566b6a20b5f482d9894daeab5
Pablo Martins <pablomartins1515@hotmail.com>
2022-12-26 12:10:44 +0000

    folder structure

---
+++ src/Controllers/UsersController.js
@@ -0,0 +1,24 @@
+const { hash } = require("bcrypt")
+const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite")
+
+class UsersController {
+
+  async create(request, response) {
+    const { name, email, password } = request.body
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    if (checkUserExists) {
+      throw new AppError("Este E-mail está em uso!")
+    }
+    if (!name) {
+      throw new AppError("Nome é obrigatório")
+    }
+    const hashedPassword = await hash(password, 8)
+    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword])
+
+    return response.status(201).json()
+  }
+}
+
+module.exports = UsersController
